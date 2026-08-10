79d2a3cc9dfbd0a24d677e254b46a975c45697a1
Alberto Panu <alberto@panu.it>
2026-08-10 13:46:56 +0200

    Add comments to gpiocheck.sh for clarity
    Added comments to explain the purpose of the script.

--- gpiocheck.sh
+++ gpiocheck.sh
@@ -1,3 +1,5 @@
+#This is a bash script to search gpio pins on gpio industrial pc boards.
+#Sometimes the datasheet pins is wrong or there is not board datasheet, you can use this script wit a DMM to test the board and find the correct pin port couple.
 LSMOD=$(lsmod)
 if [[ ${LSMOD} == *"gpio_it87"* ]]
 then
